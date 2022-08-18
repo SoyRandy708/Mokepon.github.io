2546629587a19aef663ff1a35565a8bad58e9ce7
SoyRandy708 <randy708ochoa@gmail.com>
2022-08-18 16:14:08 -0500

    Ahora el jugaodr selecciona un ataque y el enemigo selecciona uno aleatorio y se guardan en variables globales

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,15 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById("hipodoge")
@@ -20,12 +29,12 @@ function seleccionarMascotaJugador() {
     seleccionarMascotaEnemigo()
 }
 function seleccionarMascotaEnemigo() {
-    let ataqueAleatorio = aleatorio(1, 3)
+    let mascotaAleatoria = aleatorio(1, 3)
     let spanMascotaEnemigo = document.getElementById("mascota-enemiga")
 
-    if(ataqueAleatorio == 1) {
+    if(mascotaAleatoria == 1) {
         spanMascotaEnemigo.innerHTML = "Hipodoge"
-    } else if(ataqueAleatorio == 2) {
+    } else if(mascotaAleatoria == 2) {
         spanMascotaEnemigo.innerHTML = "Capipepo"
     } else {
         spanMascotaEnemigo.innerHTML = "Ratigueya"
@@ -34,6 +43,28 @@ function seleccionarMascotaEnemigo() {
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+function ataqueFuego() { 
+    ataqueJugador = "FUEGO"
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+    ataqueJugador = "AGUA"
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra() {
+    ataqueJugador = "TIERRA"
+    ataqueAleatorioEnemigo()
+}
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3)
 
+    if(ataqueAleatorio == 1) {
+        ataqueEnemigo = "FUEGO"
+    } else if(ataqueAleatorio == 2) {
+        ataqueEnemigo = "AGUA"
+    } else {
+        ataqueEnemigo = "TIERRA"
+    }
+}
 
 window.addEventListener("load", iniciarJuego)

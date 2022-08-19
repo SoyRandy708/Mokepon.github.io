a221ca735dead59163127ac27c0e0193380c58e4
SoyRandy708 <randy708ochoa@gmail.com>
2022-08-19 15:50:50 -0500

    Ahora las partes del juego estan ocultas y solo apareceran cuando tengan deben parecer

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,6 +4,11 @@ let vidasJugador = 3
 let vidasEnemigo = 3
 
 function iniciarJuego() {
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display = "none"
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display = "none"
+
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 
@@ -18,6 +23,11 @@ function iniciarJuego() {
     botonReiniciar.addEventListener("click", reiniciarJuego)
 }
 function seleccionarMascotaJugador() {
+    let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
+    sectionSeleccionarMascota.style.display = "none"
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display = "block"
+
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
@@ -32,6 +42,10 @@ function seleccionarMascotaJugador() {
     } else {
         alert("Selecciona una mascota")
     }
+    
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.disabled = true
+
     seleccionarMascotaEnemigo()
 }
 function seleccionarMascotaEnemigo() {
@@ -110,6 +124,9 @@ function crearMensajeFinal(resultadoFinal) {
     botonAgua.disabled = true
     let botonTierra = document.getElementById("boton-tierra")
     botonTierra.disabled = true
+
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display = "block"
 }
 function revisarVidas() {
     if(vidasEnemigo == 0) {

1805b6d049e7de27955c7afa03fe82a61721ec57
SoyRandy708 <randy708ochoa@gmail.com>
2022-08-26 20:05:29 -0500

    Agregue a 3 nuevos personajes al juego

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -2,16 +2,11 @@ const sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
 const sectionReiniciar = document.getElementById("reiniciar")
 const botonMascotaJugador = document.getElementById("boton-mascota")
 const botonReiniciar = document.getElementById("boton-reiniciar")
-
 const sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
-
 const spanMascotaJugador = document.getElementById("mascota-jugador")
-
 const spanMascotaEnemigo = document.getElementById("mascota-enemiga")
-
 const spanVidasJugador = document.getElementById("vidas-jugador")
 const spanVidasEnemigo = document.getElementById("vidas-enemigo")
-
 const sectionMensajes = document.getElementById("resultado")
 const ataquesDelJugador = document.getElementById("ataques-del-jugador")
 const ataquesDelEnemigo = document.getElementById("ataques-del-enemigo")
@@ -25,6 +20,9 @@ let opcionDeMokepones
 let inputHipodoge
 let inputCapipepo
 let inputRatigueya 
+let inputPydos 
+let inputLangostelvis
+let inputTucapalma
 let mascotaJugador
 let ataquesMokepon
 let ataquesMokeponEnemigo
@@ -47,11 +45,13 @@ class Mokepon {
         this.ataques = []
     }
 }
+
 let hipodoge = new Mokepon("Hipodoge", "./Assets/mokepons_mokepon_hipodoge_attack.webp", 5)
-
-let capipepo = new Mokepon("Capipepo", "./Assets/mokepons_mokepon_capipepo_attack.webp", 4)
-
-let ratigueya = new Mokepon("Ratigueya", "./Assets/mokepons_mokepon_ratigueya_attack.png", 3)
+let capipepo = new Mokepon("Capipepo", "./Assets/mokepons_mokepon_capipepo_attack.webp", 5)
+let ratigueya = new Mokepon("Ratigueya", "./Assets/mokepons_mokepon_ratigueya_attack.png", 5)
+let pydos = new Mokepon("Pydos", "./Assets/mokepons_mokepon_pydos_attack.png", 5)
+let langostelvis = new Mokepon("Langostelvis", "./Assets/mokepons_mokepon_langostelvis_attack.webp", 5)
+let tucapalma = new Mokepon("Tucapalma", "./Assets/mokepons_mokepon_tucapalma_attack.png", 5)
 
 hipodoge.ataques.push(
     {nombre: "💧", id: "boton-agua"},
@@ -74,12 +74,32 @@ ratigueya.ataques.push(
     {nombre: "💧", id: "boton-agua"},
     {nombre: "🌱", id: "boton-tierra"},
 )
-mokepones.push(hipodoge, capipepo, ratigueya)
+pydos.ataques.push(
+    {nombre: "🌱", id:"boton-tierra"},
+    {nombre: "🌱", id:"boton-tierra"},
+    {nombre: "🔥", id:"boton-fuego"},
+    {nombre: "🔥", id:"boton-fuego"},
+    {nombre: "🔥", id:"boton-fuego"},
+)
+langostelvis.ataques.push(
+    {nombre: "🔥", id:"boton-fuego"},
+    {nombre: "🔥", id:"boton-fuego"},
+    {nombre: "🔥", id:"boton-fuego"},
+    {nombre: "💧", id:"boton-agua"},
+    {nombre: "💧", id:"boton-agua"},
+)
+tucapalma.ataques.push(
+    {nombre: "💧", id:"boton-agua"},
+    {nombre: "💧", id:"boton-agua"},
+    {nombre: "🌱", id:"boton-tierra"},
+    {nombre: "🌱", id:"boton-tierra"},
+    {nombre: "🌱", id:"boton-tierra"},
+)
+mokepones.push(hipodoge, capipepo, ratigueya, pydos, langostelvis, tucapalma)
 
 
 function iniciarJuego() {
     sectionSeleccionarAtaque.style.display = "none"
-    sectionReiniciar.style.display = "none"
 
     mokepones.forEach((mokepon) => {
         opcionDeMokepones = `
@@ -94,10 +114,12 @@ function iniciarJuego() {
         inputHipodoge = document.getElementById("Hipodoge")
         inputCapipepo = document.getElementById("Capipepo")
         inputRatigueya = document.getElementById("Ratigueya")
+        inputPydos = document.getElementById("Pydos")
+        inputLangostelvis = document.getElementById("Langostelvis")
+        inputTucapalma = document.getElementById("Tucapalma")
     })
 
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
-
     botonReiniciar.addEventListener("click", reiniciarJuego)
 }
 function seleccionarMascotaJugador() {
@@ -113,11 +135,18 @@ function seleccionarMascotaJugador() {
     } else if(inputRatigueya.checked) {
         spanMascotaJugador.innerHTML = inputRatigueya.id
         mascotaJugador = inputRatigueya.id
+    } else if(inputPydos.checked) {
+        spanMascotaJugador.innerHTML = inputPydos.id
+        mascotaJugador = inputPydos.id
+    } else if(inputLangostelvis.checked) {
+        spanMascotaJugador.innerHTML = inputLangostelvis.id
+        mascotaJugador = inputLangostelvis.id
+    } else if(inputTucapalma.checked) {
+        spanMascotaJugador.innerHTML = inputTucapalma.id
+        mascotaJugador = inputTucapalma.id
     } else {
         alert("Selecciona una mascota")
-    }
-    
-    botonMascotaJugador.disabled = true
+    } 
 
     extraerAtaques(mascotaJugador)
     seleccionarMascotaEnemigo()
@@ -128,7 +157,6 @@ function extraerAtaques(mascotaJugador) {
         if(mascotaJugador === mokepones[i].nombre) {
             ataques = mokepones[i].ataques
         }
-
     }
     mostrarAtaques(ataques)
 }
@@ -164,6 +192,7 @@ function secuenciaAtaque() {
                 boton.style.background = "#0096FF"   
                 boton.disabled = true            
             }
+
             ataqueAleatorioEnemigo()
         })
     })
@@ -175,9 +204,7 @@ function seleccionarMascotaEnemigo() {
     ataquesMokeponEnemigo = mokepones[mascotaAleatoria].ataques
     secuenciaAtaque()
 }
-function aleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
+
 function ataqueAleatorioEnemigo() {
     let ataqueAleatorio = aleatorio(0, ataquesMokeponEnemigo.length - 1)
     if(ataqueAleatorio == 0 || ataqueAleatorio == 1) {
@@ -229,15 +256,7 @@ function crearMensaje(resultado) {
     ataquesDelJugador.appendChild(nuevoAtaqueDelJugador)
     ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
 }
-function crearMensajeFinal(resultadoFinal) {
-    sectionMensajes.innerHTML = resultadoFinal
 
-    botonFuego.disabled = true
-    botonAgua.disabled = true
-    botonTierra.disabled = true
-
-    sectionReiniciar.style.display = "block"
-}
 function revisarVidas() {
     if(victoriasJugador === victoriasEnemigo) {
         crearMensajeFinal("ESTO FUE UN EMPATE")
@@ -246,6 +265,16 @@ function revisarVidas() {
     } else {
         crearMensajeFinal("LO SIENTO, HAS PERDIDO")
     }
+}
+function crearMensajeFinal(resultadoFinal) {
+    sectionMensajes.innerHTML = resultadoFinal
+
+    botonFuego.disabled = true
+    botonAgua.disabled = true
+    botonTierra.disabled = true
+}
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 function reiniciarJuego() {
     location.reload()

9704ddabba0dd1a7459f846122f64516cae986b3
SoyRandy708 <randy708ochoa@gmail.com>
2022-09-02 19:02:10 -0500

    Añadi Node, Express y otras cosas al juego, ahora podemos ver el nombre del mokepon elegido y se le asigna un id

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -15,6 +15,7 @@ const contenedorAtaques = document.getElementById("contenedor-ataques")
 const sectionVerMapa = document.getElementById("ver-mapa")
 const mapa = document.getElementById("mapa")
 
+let jugadorId = null
 let mokepones = []
 let ataqueJugador = []
 let ataqueEnemigo = []
@@ -248,6 +249,20 @@ function iniciarJuego() {
 
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
     botonReiniciar.addEventListener("click", reiniciarJuego)
+
+    unirseAlJuego()
+}
+function unirseAlJuego() {
+    fetch("http://localhost:8080/unirse")
+        .then(function (res) {
+            if(res.ok) {
+                res.text()
+                    .then(function(respuesta) {
+                        console.log(respuesta)
+                        jugadorId = respuesta
+                    })
+            }  
+        })
 }
 function seleccionarMascotaJugador() {
     sectionSeleccionarMascota.style.display = "none"
@@ -275,9 +290,22 @@ function seleccionarMascotaJugador() {
         alert("Selecciona una mascota")
     } 
 
+    seleccionarMokepon(mascotaJugador)
+
     extraerAtaques(mascotaJugador)
     iniciarMapa()
     tipoAtaque()
+}
+function seleccionarMokepon(mascotaJugador) {
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`, { 
+        method: "post",
+        headers: { 
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 
+            mokepon: mascotaJugador
+        })
+    })
 }
 function tipoAtaque() {
     if(mascotaJugador ){
